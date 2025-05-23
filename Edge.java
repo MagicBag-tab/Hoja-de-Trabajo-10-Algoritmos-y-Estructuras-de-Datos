036e6c09b156cf3d00a0c0f92276f6683475a53a
MagicBag-tab <est24347@uvg.edu.gt>
2025-05-22 18:55:20 -0600

    Implementacion de la clase Edge, que es la arista para utilizarla en el grafo

--- Edge.java
+++ Edge.java
@@ -1,3 +1,21 @@
-public class Edge {
+/*
+ * Implementación de la clase de arista para ser utilizada en la clase del grafo.
+ * 
+ * @date 22/05/2025
+ * 
+ * @reference Código sacado del material de Clase de Algoritmos y Estructura de datos
+ * 
+ */
+class Edge {
+    int to;
+    int weight;
 
+    /*
+     * Constructor de la arista que muestra su peso y a donde va.
+     */
+
+    Edge(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
+    }
 }
